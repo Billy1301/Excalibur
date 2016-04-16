012a69595ec456f71cd5c057f1a8b549dd3c642f
Billy <Billy@BILLs-MacBook-Pro.local>
2016-04-16 16:41:12 -0700

    worked on fragment codes

--- Excalibur/app/src/main/java/com/example/billy/excalibur/MainActivity.java
+++ Excalibur/app/src/main/java/com/example/billy/excalibur/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.billy.excalibur;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,34 +14,81 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.billy.excalibur.fragment.ArticleStory;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    FrameLayout fragContainer;
+    NavigationView navigationView;
+    FloatingActionButton fab;
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    ArticleStory articleFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        setViews();
+        setFAB();
+        setActionBarDrawer();
+        navigationView.setNavigationItemSelectedListener(this);
+        setFragment();
+
+
+
+    }
+
+
+    public void setViews(){
+        fragContainer = (FrameLayout)findViewById(R.id.frag_container);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        fragmentManager = getSupportFragmentManager();
+        articleFragment = new ArticleStory();
+
+    }
+
+
+    //This will need to setup with the RecycleView Click Listener
+    public void setFragment(){
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frag_container, articleFragment);
+        fragmentTransaction.commit();
+    }
+
+
+
+
+    public void setActionBarDrawer(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    public void setFAB(){
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
