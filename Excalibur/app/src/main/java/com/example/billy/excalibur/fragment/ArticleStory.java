0bbcaf4201876e1e3b4444d1d1844f1974e570f8
Billy <Billy@BILLs-MacBook-Pro.local>
2016-04-16 15:37:12 -0700

    created fragment for article

---
+++ Excalibur/app/src/main/java/com/example/billy/excalibur/fragment/ArticleStory.java
@@ -0,0 +1,36 @@
+package com.example.billy.excalibur.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.billy.excalibur.R;
+
+/**
+ * Created by Billy on 4/16/16.
+ */
+public class ArticleStory extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.article_activity_fragment, container, false);
+
+        ImageView image = (ImageView)v.findViewById(R.id.article_image_view);
+        TextView articleTitle = (TextView)v.findViewById(R.id.article_headline_title);
+        TextView aurthurName = (TextView)v.findViewById(R.id.article_aurthur);
+        TextView articleInfo = (TextView)v.findViewById(R.id.article_story);
+
+
+
+
+
+        return v;
+    }
+}
