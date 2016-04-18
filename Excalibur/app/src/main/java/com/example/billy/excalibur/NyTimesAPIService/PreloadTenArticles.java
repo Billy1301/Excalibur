bad267119f1dfee676eb6d62d74a6907104c300a
Peter John Martinez <peterjohn.martinez@gmail.com>
2016-04-18 14:42:38 -0700

    preloadTenArticles compiles

--- Excalibur/app/src/main/java/com/example/billy/excalibur/NyTimesAPIService/PreloadTenArticles.java
+++ Excalibur/app/src/main/java/com/example/billy/excalibur/NyTimesAPIService/PreloadTenArticles.java
@@ -6,9 +6,10 @@ package com.example.billy.excalibur.NyTimesAPIService;
 public class PreloadTenArticles {
 
     public static NewsWireObjects getPreloadArticle(int i){
+        NewsWireObjects article;
         switch (i) {
             case 0:
-                NewsWireObjects article = new NewsWireObjects(
+                article = new NewsWireObjects(
                         "Business",
                         "2016 Pulitzer Prize Winners",
                         "http://www.nytimes.com/interactive/2016/04/19/business/media/pulitzer-prize-winners-complete-list.html",
@@ -16,10 +17,15 @@ public class PreloadTenArticles {
                         "The Pulitzers are in their centennial year, and the winners announced on Monday reflected in part the changes sweeping the media landscape. Here is the full list."
                 );
                 return article;
-            case 1:
 
             default:
-
+                article = new NewsWireObjects(
+                        "Business",
+                        "2016 Pulitzer Prize Winners",
+                        "http://www.nytimes.com/interactive/2016/04/19/business/media/pulitzer-prize-winners-complete-list.html",
+                        "https://static01.nyt.com/images/2016/04/18/blogs/19pulitzers1alt/18-lens-refugees-slide-6010-thumbStandard.jpg",
+                        "The Pulitzers are in their centennial year, and the winners announced on Monday reflected in part the changes sweeping the media landscape. Here is the full list."
+                );
                 return article;
         }
     }
