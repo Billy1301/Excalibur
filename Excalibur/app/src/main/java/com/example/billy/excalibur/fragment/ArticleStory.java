a2062826151fea8390a102165f3cc15e9fe4203a
Peter John Martinez <peterjohn.martinez@gmail.com>
2016-04-20 12:04:22 -0700

    indeterminate progress bar added

--- Excalibur/app/src/main/java/com/example/billy/excalibur/fragment/ArticleStory.java
+++ Excalibur/app/src/main/java/com/example/billy/excalibur/fragment/ArticleStory.java
@@ -1,6 +1,7 @@
 package com.example.billy.excalibur.fragment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,10 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.billy.excalibur.R;
@@ -23,24 +26,28 @@ import com.squareup.picasso.Picasso;
 public class ArticleStory extends Fragment {
 
     private static final String TAG = "ArticleStory Fragment";
+    private ProgressBar progress;
+    private WebView articleWebView;
 
     /**
-     *  user interface to callback for fragment
-     *
+     * user interface to callback for fragment
      */
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.article_activity_fragment, container, false);
-        WebView articleWebView = (WebView) v.findViewById(R.id.article_web_view);
+        articleWebView = (WebView) v.findViewById(R.id.article_web_view);
+
 
         Bundle article = getArguments(); //retrieves the article object split into 5 fields of a string array
         String[] articleDetails = article.getStringArray("article");
 
+        progress = (ProgressBar) v.findViewById(R.id.progress_bar);
+
         WebSettings webSettings = articleWebView.getSettings();
+        articleWebView.setWebViewClient(new WebViewClientDemo()); //opens url in app, not in default browser
         webSettings.setJavaScriptEnabled(true); //turn js on for hacking and giving better ux
-        articleWebView.setWebViewClient(new WebViewClient()); //opens url in app, not in default browser
         articleWebView.loadUrl(articleDetails[2]);
 
         Log.i(TAG, articleDetails[0]);
@@ -50,5 +57,33 @@ public class ArticleStory extends Fragment {
         Log.i(TAG, articleDetails[4]);
 
         return v;
+
     }
+
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            progress.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progress.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progress.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
+
+
+
+
