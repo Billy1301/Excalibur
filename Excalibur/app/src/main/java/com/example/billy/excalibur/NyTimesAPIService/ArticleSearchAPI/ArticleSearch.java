1fc277b23e6393b84d9c06899e3bbb0d1f9c41d5
Michael Muccio <michaelmuccio@gmail.com>
2016-04-21 16:53:36 -0700

    refactoring API classes. erased unsused setters and updated searched article icon index to 0

--- Excalibur/app/src/main/java/com/example/billy/excalibur/NyTimesAPIService/ArticleSearchAPI/ArticleSearch.java
+++ Excalibur/app/src/main/java/com/example/billy/excalibur/NyTimesAPIService/ArticleSearchAPI/ArticleSearch.java
@@ -9,10 +9,6 @@ public class ArticleSearch {
     public Response getResponse() {
         return response;
     }
-
-    public void setResponse(Response response) {
-        this.response = response;
-    }
 }
 
 
