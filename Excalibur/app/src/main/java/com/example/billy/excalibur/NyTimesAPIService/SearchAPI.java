ffaeb6a183728adab0e53734cd56bf1089ace60d
Peter John Martinez <peterjohn.martinez@gmail.com>
2016-04-21 16:35:39 -0700

    few xml

--- Excalibur/app/src/main/java/com/example/billy/excalibur/NyTimesAPIService/SearchAPI.java
+++ Excalibur/app/src/main/java/com/example/billy/excalibur/NyTimesAPIService/SearchAPI.java
@@ -3,7 +3,7 @@ package com.example.billy.excalibur.NyTimesAPIService;
 
 
 
-import com.example.billy.excalibur.Keys.Keys;
+import com.example.billy.excalibur.keys.Keys;
 import com.example.billy.excalibur.NyTimesAPIService.ArticleSearchAPI.ArticleSearch;
 
 import retrofit2.Call;
